ce8ad0fe03028e6e3c05200a70fc61b29102b763
George <george.gelmetdinov@gmail.com>
2023-03-14 19:46:41 +0300

    committed

---
+++ src/main/java/com/svigel/pokerclub/PokerclubApplication.java
@@ -0,0 +1,13 @@
+package com.svigel.pokerclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerclubApplication.class, args);
+	}
+
+}

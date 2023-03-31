f0fdd133f5537b32750945b516fb8e5bf8831ed9
Egor Sviridenko <egoros21sviridos@gmail.com>
2023-03-31 21:18:53 +0300

    Added getRoom

--- src/main/java/com/svigel/pokerclub/controller/RoomController.java
+++ src/main/java/com/svigel/pokerclub/controller/RoomController.java
@@ -4,6 +4,8 @@ package com.svigel.pokerclub.controller;
 import com.svigel.pokerclub.service.RoomService;
 import com.svigel.pokerclub.web.RoomDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +17,8 @@ public class RoomController {
         this.roomService = roomService;
     }
 
-    public RoomDTO getRoomInfo(Long id) {
+    @GetMapping("/room/{id}")
+    public RoomDTO getRoomInfo(@PathVariable Long id) {
         return RoomDTO.fromRoom(roomService.getRoom(id));
     }
 
